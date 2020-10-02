85a33403ddf53858dce208ad3a644d22dd40379d
stu mckenzie <stu.mckenzie@protonmail.com>
2020-10-02 10:51:47 +1000

    Converting igneous.blastX project from [.NET Framework] to [.NET Standard 2.0]

---
+++ src/igneous.blastx/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("blastX")]
+[assembly: AssemblyDescription("BlastX Integration")]
+[assembly: AssemblyCompany("Igneous Tech")]
+[assembly: AssemblyProduct("blastX")]
+[assembly: AssemblyCopyright("Copyright © Igneous Tech 2020")]
+[assembly: Obfuscation(Feature = "rule=features:RenameParams;pattern:*.ctor", Exclude = true)]
+[assembly: ComVisible(false)]
+[assembly: Guid("5edbe7b8-2e9d-425f-865b-f14d9f78b83d")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
